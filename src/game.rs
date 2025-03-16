6ef505af43252ddabd3a5e13ca89dc39e1b4e9d8
Borys Kopeć <boryskopec00@gmail.com>
2025-03-16 18:39:30 +0100

    Implement a simple dash mechanic

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,10 @@
 use super::CursorPosition;
 use super::GameState;
 use super::utils::*;
-use bevy::color::palettes::css::{GREY, RED};
+use bevy::color::palettes::css::{BLUE, GREY, RED};
 use bevy::prelude::*;
 
-const PLAYER_SPEED: f32 = 100.0;
+const PLAYER_SPEED: f32 = 200.0;
 /// Actually, rate of exponential decay in the distance between camera and it's goal
 const CAMERA_SPEED: f32 = 5.0;
 
@@ -20,19 +20,34 @@ pub fn game_plugin(app: &mut App) {
 
 /// Accumulates the input on `Update` schedule, is cleared and taken into account in `player_physics`,
 /// which runs on `FixedUpdate` schedule.
-#[derive(Resource)]
-struct PlayerInput(Vec3);
+#[derive(Resource, Default)]
+struct PlayerInput {
+    direction: Vec3,
+    dash: bool,
+}
 
 #[derive(Component)]
 struct Player;
 
-/// Represents the internal, underlying translation (position) used in the game logic,
-/// not on the UI level where `Transform` should be used.
-#[derive(Component, Deref, DerefMut)]
-struct InternalTranslation(Vec3);
+/// Represents the internal, underlying state used in the game logic, not on the UI level.
+#[derive(Component)]
+struct PlayerState {
+    position: Vec3,
+    dashing: bool,
+}
+impl Default for PlayerState {
+    fn default() -> Self {
+        PlayerState {
+            position: Vec3::new(0.0, 0.0, 1.0),
+            dashing: false,
+        }
+    }
+}
 
 fn enter_game(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
-    commands.insert_resource(PlayerInput(Vec3::ZERO));
+    commands.init_resource::<PlayerInput>();
+    commands.insert_resource(DashTimer(Timer::from_seconds(0.5, TimerMode::Once)));
+
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::new(1280., 720.))),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(GREY))),
@@ -42,7 +57,7 @@ fn enter_game(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut mate
         Player,
         Mesh2d(meshes.add(Circle::new(25.))),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(RED))),
-        InternalTranslation(Vec3::new(0.0, 0.0, 1.0)),
+        PlayerState::default(),
         Transform::default(),
         StateScoped(GameState::Game),
     ));
@@ -72,22 +87,59 @@ fn handle_player_input(keyboard: Res<ButtonInput<KeyCode>>, mut player_input: Re
         (false, true, true, false) => DIRECTION_DOWNRIGHT,
     };
 
-    player_input.0 += direction;
+    if keyboard.any_just_pressed(vec![KeyCode::ShiftLeft, KeyCode::Space]) {
+        player_input.dash = true
+    }
+
+    player_input.direction += direction;
 }
+
+#[derive(Resource, Deref, DerefMut)]
+struct DashTimer(Timer);
 
 fn player_physics(
     time_fixed: Res<Time<Fixed>>,
-    mut query: Query<&mut InternalTranslation, With<Player>>,
+    mut query: Query<&mut PlayerState, With<Player>>,
     mut player_input: ResMut<PlayerInput>,
+    mut dash_timer: ResMut<DashTimer>,
 ) {
-    let mut position = query.single_mut();
-    position.0 += player_input.0 * PLAYER_SPEED * time_fixed.delta_secs();
-    player_input.0 = Vec3::ZERO;
+    let mut state = query.single_mut();
+
+    let dt = time_fixed.delta();
+
+    // conscious short circuit
+    if state.dashing && dash_timer.0.tick(dt).finished() {
+        state.dashing = false;
+        dash_timer.0.reset();
+    }
+
+    if player_input.dash {
+        state.dashing = player_input.dash
+    }
+
+    player_input.dash = false;
+
+    // double the speed when dashing
+    let speed_mult = 1.0 + f32::from(state.dashing);
+
+    state.position += player_input.direction * speed_mult * PLAYER_SPEED * dt.as_secs_f32();
+    player_input.direction = Vec3::ZERO;
 }
 
-fn update_player(mut query: Query<(&mut Transform, &InternalTranslation), With<Player>>) {
-    let (mut transform, position) = query.single_mut();
-    transform.translation = position.0;
+fn update_player(
+    mut query: Query<(&mut Transform, &PlayerState, &mut MeshMaterial2d<ColorMaterial>), With<Player>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let (mut transform, state, mut material) = query.single_mut();
+
+    // TODO Introduce reusing colors instead of adding new ones.
+    if state.dashing {
+        material.0 = materials.add(ColorMaterial::from_color(BLUE));
+    } else {
+        material.0 = materials.add(ColorMaterial::from_color(RED));
+    }
+
+    transform.translation = state.position;
 }
 
 fn update_camera(
