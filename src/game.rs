6aa0803bdc8f43192d0038f96a16f0c19d86eb83
Borys Kopeć <boryskopec00@gmail.com>
2025-03-18 22:34:26 +0100

    Add simple primary attack handling, also generalize physics to projectiles

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,5 @@
+// TODO Consider merging `Position` and `Tranfsorm`, since it may just not be worth it to run the physics separately
+
 use super::CursorPosition;
 use super::GameState;
 use super::utils::*;
@@ -6,16 +8,21 @@ use bevy::prelude::*;
 
 const PLAYER_SPEED: f32 = 250.0;
 /// Actually, rate of exponential decay in the distance between camera and it's goal
-const CAMERA_SPEED: f32 = 5.0;
+const CAMERA_SPEED: f32 = 6.0;
 const CURSOR_CAMERA_INFLUENCE: f32 = 0.4;
 
 pub fn game_plugin(app: &mut App) {
     app.add_systems(OnEnter(GameState::Game), enter_game)
-        .add_systems(FixedUpdate, player_physics.run_if(in_state(GameState::Game)))
         .add_systems(Update, handle_player_input.run_if(in_state(GameState::Game)))
-        .add_systems(Update, update_player.run_if(in_state(GameState::Game)))
+        .add_systems(Update, display.run_if(in_state(GameState::Game)))
+        .add_systems(Update, primary.run_if(in_state(GameState::Game)))
         .add_systems(Update, update_camera.run_if(in_state(GameState::Game)))
         .add_systems(Update, exit_game_check.run_if(in_state(GameState::Game)))
+        .add_systems(
+            FixedUpdate,
+            // important to keep the ordering so the movement is smooth
+            (player_state, physics).chain().run_if(in_state(GameState::Game)),
+        )
         .add_systems(OnExit(GameState::Game), exit_game);
 }
 
@@ -25,24 +32,25 @@ pub fn game_plugin(app: &mut App) {
 struct PlayerInput {
     direction: Vec3,
     dash: bool,
+    primary: bool,
 }
 
+#[derive(Component)]
+struct Projectile;
 #[derive(Component)]
 struct Player;
 
-/// Represents the internal, underlying state used in the game logic, not on the UI level.
 #[derive(Component)]
+struct Velocity(Vec3);
+#[derive(Component)]
+struct Position(Vec3);
+
+/// Represents the internal, underlying state used in the game logic, not on the UI level.
+#[derive(Default, Component)]
 struct PlayerState {
-    position: Vec3,
+    // TODO maybe handle dashing / primary with a union since those should be exclusive
     dashing: bool,
-}
-impl Default for PlayerState {
-    fn default() -> Self {
-        PlayerState {
-            position: Vec3::new(0.0, 0.0, 1.0),
-            dashing: false,
-        }
-    }
+    primary: bool,
 }
 
 fn enter_game(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
@@ -59,6 +67,8 @@ fn enter_game(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut mate
         Mesh2d(meshes.add(Circle::new(25.))),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(RED))),
         PlayerState::default(),
+        Position(Vec3::from((0.0, 0.0, 1.0))),
+        Velocity(Vec3::ZERO),
         Transform::default(),
         StateScoped(GameState::Game),
     ));
@@ -67,7 +77,11 @@ fn exit_game(mut commands: Commands) {
     commands.remove_resource::<PlayerInput>();
 }
 
-fn handle_player_input(keyboard: Res<ButtonInput<KeyCode>>, mut player_input: ResMut<PlayerInput>) {
+fn handle_player_input(
+    mouse: Res<ButtonInput<MouseButton>>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut player_input: ResMut<PlayerInput>,
+) {
     let left = keyboard.pressed(KeyCode::KeyA);
     let right = keyboard.pressed(KeyCode::KeyD);
     let down = keyboard.pressed(KeyCode::KeyS);
@@ -87,24 +101,24 @@ fn handle_player_input(keyboard: Res<ButtonInput<KeyCode>>, mut player_input: Re
         (false, false, true, false) | (true, true, true, false) => DIRECTION_DOWN,
         (false, true, true, false) => DIRECTION_DOWNRIGHT,
     };
-
-    if keyboard.any_just_pressed(vec![KeyCode::ShiftLeft, KeyCode::Space]) {
-        player_input.dash = true
-    }
-
     player_input.direction += direction;
+
+    // TODO Investigate whether this causes problems if Update runs twice with no FixedUpdate
+    player_input.dash = keyboard.any_just_pressed(vec![KeyCode::ShiftLeft, KeyCode::Space]);
+    player_input.primary = mouse.pressed(MouseButton::Left);
 }
 
-#[derive(Resource, Deref, DerefMut)]
+#[derive(Resource)]
 struct DashTimer(Timer);
 
-fn player_physics(
+// TODO Split into three stages: input handling (UI) -> state (Fixed) -> physics (Fixed) -> UI
+fn player_state(
     time_fixed: Res<Time<Fixed>>,
-    mut query: Query<&mut PlayerState, With<Player>>,
-    mut player_input: ResMut<PlayerInput>,
+    mut query: Query<(&mut PlayerState, &mut Velocity), With<Player>>,
+    mut input: ResMut<PlayerInput>,
     mut dash_timer: ResMut<DashTimer>,
 ) {
-    let mut state = query.single_mut();
+    let (mut state, mut velocity) = query.single_mut();
 
     let dt = time_fixed.delta();
 
@@ -114,24 +128,59 @@ fn player_physics(
         dash_timer.0.reset();
     }
 
-    if player_input.dash {
-        state.dashing = player_input.dash
+    if input.dash {
+        state.dashing = input.dash
     }
 
-    player_input.dash = false;
+    input.dash = false;
+
+    state.primary = input.primary;
 
     // double the speed when dashing
     let speed_mult = 1.0 + f32::from(state.dashing);
 
-    state.position += player_input.direction * speed_mult * PLAYER_SPEED * dt.as_secs_f32();
-    player_input.direction = Vec3::ZERO;
+    velocity.0 = input.direction * speed_mult * PLAYER_SPEED;
+    // handled all input that accumulated since last `Update`
+    input.direction = Vec3::ZERO;
 }
 
-fn update_player(
-    mut query: Query<(&mut Transform, &PlayerState, &mut MeshMaterial2d<ColorMaterial>), With<Player>>,
+fn physics(time_fixed: Res<Time<Fixed>>, mut query: Query<(&mut Position, &Velocity)>) {
+    query
+        .iter_mut()
+        .for_each(|(mut pos, vel)| pos.0 += vel.0 * time_fixed.delta_secs());
+}
+
+fn primary(
+    mut commands: Commands,
+    query: Query<(&Position, &PlayerState), With<Player>>,
+    cursor_position: Res<CursorPosition>,
+    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let (mut transform, state, mut material) = query.single_mut();
+    let (position, player_state) = query.single();
+
+    // TODO Despawn bullets after some time
+    if player_state.primary {
+        commands.spawn((
+            Projectile,
+            Mesh2d(meshes.add(Circle::new(5.))),
+            MeshMaterial2d(materials.add(ColorMaterial::from_color(BLUE))),
+            Transform::from_translation(position.0),
+            Position(position.0),
+            // TODO add handling when the cursor is not on the screen
+            Velocity(cursor_position.0.unwrap().extend(0.0) - position.0),
+            StateScoped(GameState::Game),
+        ));
+    }
+}
+
+/// Updates the visible components based on the physical state.
+fn display(
+    mut player: Query<(&PlayerState, &mut MeshMaterial2d<ColorMaterial>), With<Player>>,
+    mut query: Query<(&Position, &mut Transform)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let (state, mut material) = player.single_mut();
 
     // TODO Introduce reusing colors instead of adding new ones.
     if state.dashing {
@@ -140,7 +189,9 @@ fn update_player(
         material.0 = materials.add(ColorMaterial::from_color(RED));
     }
 
-    transform.translation = state.position;
+    query
+        .iter_mut()
+        .for_each(|(pos, mut transform)| transform.translation = pos.0);
 }
 
 fn update_camera(
