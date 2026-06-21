1108ba7e623019970267a1d2ba727a8f9af5834a
Borys Kopeć <boryskopec00@gmail.com>
2026-06-21 22:54:28 +0200

    MRE v5

--- src/menu.rs
+++ src/menu.rs
@@ -12,7 +12,6 @@ use bevy::window::WindowMode;
 pub fn plugin(app: &mut App) {
     app.init_state::<MenuSubState>()
         .add_systems(OnEnter(MenuSubState::Main), main_ui)
-        .add_systems(Update, update_interacted_buttons_display)
         .add_systems(OnEnter(MenuSubState::Settings), settings_ui);
 }
 
@@ -21,7 +20,7 @@ fn main_ui(mut commands: Commands) -> Result {
         .spawn((ui::typical_parent_node(), DespawnOnExit(MenuSubState::Main)))
         .with_children(|parent| {
             parent.spawn((
-                MyButton,
+                Button,
                 Text::new("Settings"),
                 observe(
                     |_: On<Activate>, mut next_substate: ResMut<NextState<MenuSubState>>| {
@@ -40,68 +39,36 @@ fn settings_ui(mut commands: Commands) {
             DespawnOnExit(MenuSubState::Settings),
         ))
         .with_children(|parent| {
-            parent.spawn(Node {
-                width: Val::Percent(100.0),
-                height: Val::Percent(80.0),
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::FlexStart,
-                align_items: AlignItems::Center,
-                ..Default::default()
-            });
-            parent
-                .spawn(Node {
-                    width: Val::Percent(100.0),
-                    justify_content: JustifyContent::SpaceEvenly,
-                    ..Default::default()
-                })
-                .with_children(|parent| {
-                    parent.spawn((
-                        MyButton,
-                        Text::new("Menu"),
-                        observe(
-                            |_: On<Activate>, mut next_substate: ResMut<NextState<MenuSubState>>| {
-                                info!("Menu");
-                                next_substate.set(MenuSubState::Main)
-                            },
-                        ),
-                    ));
-                    parent.spawn((
-                        MyButton,
-                        Text::new("Apply"),
-                        observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
-                            info!("Apply");
-                            let mut bevy_window =
-                                q_window.single_mut().expect("expected exactly one window");
-                            bevy_window.mode = WindowMode::Windowed;
-                            bevy_window
-                                .resolution
-                                .set_physical_resolution(HD_WIDTH, HD_HEIGHT);
-                            bevy_window.resolution.set_scale_factor(2.0);
-                        }),
-                    ));
-                });
+            parent.spawn((
+                Button,
+                Text::new("Menu"),
+                observe(
+                    |_: On<Activate>, mut next_substate: ResMut<NextState<MenuSubState>>| {
+                        info!("Menu");
+                        next_substate.set(MenuSubState::Main)
+                    },
+                ),
+            ));
+            parent.spawn((
+                Button,
+                Text::new("Apply"),
+                observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
+                    info!("Apply");
+                    let mut bevy_window =
+                        q_window.single_mut().expect("expected exactly one window");
+                    bevy_window.mode = WindowMode::Windowed;
+                    bevy_window
+                        .resolution
+                        .set_physical_resolution(HD_WIDTH, HD_HEIGHT);
+                    bevy_window.resolution.set_scale_factor(3.0);
+                }),
+            ));
         });
-}
-
-fn update_interacted_buttons_display(
-    mut q_buttons: Query<(&Interaction, &mut BackgroundColor), Changed<Interaction>>,
-) {
-    q_buttons.iter_mut().for_each(|(interaction, mut color)| {
-        *color = match interaction {
-            Interaction::None => BackgroundColor::DEFAULT,
-            Interaction::Hovered => BackgroundColor(Color::srgb(0.5, 0.5, 0.5)),
-            Interaction::Pressed => BackgroundColor(Color::srgb(0.5, 1.0, 0.5)),
-        };
-    });
 }
 
 #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
 enum MenuSubState {
-    #[default]
     Main,
+    #[default]
     Settings,
 }
-
-#[derive(Component)]
-#[require(Interaction, Button, BackgroundColor)]
-struct MyButton;
