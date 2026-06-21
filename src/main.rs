6187b47d9672bdaca2bf98703e31217a0f9255e7
Borys Kopeć <boryskopec00@gmail.com>
2026-06-21 23:44:05 +0200

    MRE it's about the Node logical size not moving while the resolution is changing

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use bevy::prelude::*;
 use bevy::ui_widgets::Activate;
 use bevy::ui_widgets::Button;
 use bevy::ui_widgets::observe;
-use bevy::window::WindowMode;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        //.add_plugins(bevy_inspector_egui::bevy_egui::EguiPlugin::default())
-        //.add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::default())
+        .insert_resource(GlobalUiDebugOptions {
+            enabled: true,
+            ..default()
+        })
+        .add_plugins(bevy_inspector_egui::bevy_egui::EguiPlugin::default())
+        .add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::default())
         .add_systems(Startup, startup)
         .run();
 }
@@ -24,23 +27,16 @@ fn startup(mut commands: Commands) {
             align_items: AlignItems::Center,
             ..Node::DEFAULT
         })
-        .with_children(|parent| {
-            parent.spawn((
-                Button,
-                Text::new("Menu"),
-                observe(|_: On<Activate>| info!("Menu")),
-            ));
-            parent.spawn((
-                Button,
-                Text::new("Apply"),
-                observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
-                    info!("Apply");
-                    let mut bevy_window =
-                        q_window.single_mut().expect("expected exactly one window");
-                    bevy_window.mode = WindowMode::Windowed;
-                    bevy_window.resolution.set_physical_resolution(1280, 720);
-                    bevy_window.resolution.set_scale_factor(2.0);
-                }),
-            ));
-        });
+        .with_child((
+            Button,
+            Text::new("Apply"),
+            observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
+                info!("Apply");
+                let mut bevy_window = q_window.single_mut().expect("expected exactly one window");
+                info!("{:?}", bevy_window.resolution);
+                bevy_window.resolution.set_physical_resolution(1280, 720);
+                bevy_window.resolution.set_scale_factor(2.0);
+                // bevy_window.resolution.set_scale_factor_override(Some(2.0));
+            }),
+        ));
 }
