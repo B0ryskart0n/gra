46656ff962648fdf1f8f4fdfd30574e2e205b2ec
Borys Kopeć <boryskopec00@gmail.com>
2026-06-22 23:11:06 +0200

    mre of need for double setting for bevy_winit to take effect

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,45 @@
+use bevy::input::common_conditions::input_just_pressed;
 use bevy::prelude::*;
-use bevy::ui_widgets::Activate;
-use bevy::ui_widgets::Button;
-use bevy::ui_widgets::observe;
-use bevy::window::WindowResolution;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: WindowResolution::new(640, 360).with_scale_factor_override(1.0),
-                ..default()
-            }),
-            ..default()
-        }))
-        .insert_resource(GlobalUiDebugOptions {
-            enabled: true,
-            ..default()
-        })
-        .add_plugins(bevy_inspector_egui::bevy_egui::EguiPlugin::default())
-        .add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::default())
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, startup)
+        .add_systems(
+            Update,
+            (
+                update_resolution_text,
+                change_resolution.run_if(input_just_pressed(KeyCode::Space)),
+                reset_resolution.run_if(input_just_pressed(KeyCode::Escape)),
+            ),
+        )
         .run();
 }
 
 fn startup(mut commands: Commands) {
-    commands.spawn((Camera2d,));
-    commands
-        .spawn(Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            flex_direction: FlexDirection::Column,
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            ..Node::DEFAULT
-        })
-        .with_child((
-            Button,
-            Text::new("Apply"),
-            observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
-                info!("Apply");
-                let mut bevy_window = q_window.single_mut().expect("expected exactly one window");
+    commands.spawn(Camera2d);
+    commands.spawn(Text::default());
+}
+fn reset_resolution(mut q_window: Query<&mut Window>) {
+    let mut bevy_window = q_window.single_mut().unwrap();
+    bevy_window.resolution.set_scale_factor_override(None);
+    bevy_window.resolution.set_physical_resolution(1280, 720);
+}
+fn change_resolution(mut q_window: Query<&mut Window>) {
+    let mut bevy_window = q_window.single_mut().unwrap();
+    bevy_window.resolution.set_scale_factor_override(Some(2.0));
+    bevy_window.resolution.set_physical_resolution(1280, 720);
+    // After setting the above I expect a window with HD size, but 640x360 internal resolution
+}
+/// Only for informative purposes, to display the current window state.
+fn update_resolution_text(mut q_text: Query<&mut Text>, q_window: Query<&Window>) {
+    let res = q_window.single().unwrap().resolution.clone();
 
-                // Hyprland has a bug and only when window is maximized it will take resize requests
-                // So debugging this issue is problematic on Hyprland
-                bevy_window.set_maximized(true);
-
-                info!("res_before: {:?}", bevy_window.resolution.clone());
-
-                bevy_window.resolution.set_scale_factor(0.008);
-                bevy_window.resolution.set_scale_factor_override(Some(3.0));
-                bevy_window.resolution.set_physical_resolution(1280, 360);
-
-                info!("res_after: {:?}", bevy_window.resolution);
-            }),
-        ));
+    q_text.single_mut().unwrap().0 = format!(
+        "Logical size: {}\nPhysical size: {}\nBase scale: {}\nScale override: {:?}",
+        res.size(),
+        res.physical_size(),
+        res.base_scale_factor(),
+        res.scale_factor_override()
+    );
 }
