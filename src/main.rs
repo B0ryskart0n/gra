e67d571262a39f4fc8227c552453caac65c6926a
Borys Kopeć <boryskopec00@gmail.com>
2026-06-22 19:35:50 +0200

    Final reproducible version with hyprland bug making this problematic

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ use bevy::prelude::*;
 use bevy::ui_widgets::Activate;
 use bevy::ui_widgets::Button;
 use bevy::ui_widgets::observe;
+use bevy::window::WindowResolution;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(640, 360).with_scale_factor_override(1.0),
+                ..default()
+            }),
+            ..default()
+        }))
         .insert_resource(GlobalUiDebugOptions {
             enabled: true,
             ..default()
@@ -33,10 +40,18 @@ fn startup(mut commands: Commands) {
             observe(|_: On<Activate>, mut q_window: Query<&mut Window>| {
                 info!("Apply");
                 let mut bevy_window = q_window.single_mut().expect("expected exactly one window");
-                info!("{:?}", bevy_window.resolution);
-                bevy_window.resolution.set_physical_resolution(1280, 720);
-                bevy_window.resolution.set_scale_factor(2.0);
-                // bevy_window.resolution.set_scale_factor_override(Some(2.0));
+
+                // Hyprland has a bug and only when window is maximized it will take resize requests
+                // So debugging this issue is problematic on Hyprland
+                bevy_window.set_maximized(true);
+
+                info!("res_before: {:?}", bevy_window.resolution.clone());
+
+                bevy_window.resolution.set_scale_factor(0.008);
+                bevy_window.resolution.set_scale_factor_override(Some(3.0));
+                bevy_window.resolution.set_physical_resolution(1280, 360);
+
+                info!("res_after: {:?}", bevy_window.resolution);
             }),
         ));
 }
